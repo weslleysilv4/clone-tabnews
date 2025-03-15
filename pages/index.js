129bd04a4e19ed45e47b7ac7f95f52275b43cf22
weslleysilv4 <weslleysilva.swe@gmail.com>
2025-03-15 13:33:33 -0300

    Replace external GIF with local asset and update image tag in Home component

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Mau mau, vai tomar no cu!</h1>
-      <img
-        src="https://www.icegif.com/wp-content/uploads/2022/01/icegif-982.gif"
-        alt="Vai tomar no cu"
-      />
+      <img src="/icegif.gif" alt="Vai tomar no cu" />
     </div>
   );
 }
