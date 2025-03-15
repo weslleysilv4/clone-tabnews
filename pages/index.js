fc1c9b1b66893cb1e7633270e02ddcb1fa6442b6
weslleysilv4 <weslleysilva.swe@gmail.com>
2025-03-15 13:31:34 -0300

    Add initial project setup with .gitignore, package.json, and Home component

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Mau mau, vai tomar no cu!</h1>
+      <img
+        src="https://www.icegif.com/wp-content/uploads/2022/01/icegif-982.gif"
+        alt="Vai tomar no cu"
+      />
+    </div>
+  );
+}
