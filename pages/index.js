315d371a9d356f170769e88bd2282741f1f28557
weslleysilv4 <weslleysilva.swe@gmail.com>
2025-05-16 19:17:14 -0300

    Add Next.js configuration, update package.json scripts, and create Vercel configuration

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Clone TabNews</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <h1>Mau mau, vai tomar no cu!</h1>
       <img src="/icegif.gif" alt="Vai tomar no cu" />
     </div>
